a250e33f34ff56332a2224235e8a5d4b46199f7f
Kedharnadh <adurikedharnadh@gmail.com>
2025-03-10 09:27:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,36 +2,37 @@ resource "aws_instance" "ourfirst" {
   ami           = "ami-00bb6a80f01f03502"
   instance_type = "t2.micro"
   key_name = "assignment_09"
-  security_groups = [aws_security_group.test_sg.id]
+  security_groups = ["${aws_security_group.example.name}"]
 }
 
-resource "aws_security_group" "test_sg" {
-  name = "securitygroup2"
-  description = "Allow http and ssh ports"
-  tags = {
-    Name = "terrafrom_securitygroup"
+
+resource "aws_security_group" "example" {
+  name   = "security-group"
+ ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  
   }
-  ingress {
-    from_port = "22"
-    to_port = "22"
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+   ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  
   }
 
-    ingress {  #allowing http port
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] #from anywhere
-    }
 
-    egress{
-    from_port = 0
-    to_port = 0
-    protocol = "-1" #  any port
-    cidr_blocks = ["0.0.0.0/0"] # anywhere
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
   }
 }
+
 
 provider "aws" {
   region     = "ap-south-1"
